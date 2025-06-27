becdd161b750cd6b77e6a22a5cb4a5858df53f95
bruno teixeira <brunofvteixeira@gmail.com>
2025-06-27 11:35:36 -0300

    fix

--- src/app/jogo/[slug]/components/pergunta.tsx
+++ src/app/jogo/[slug]/components/pergunta.tsx
@@ -13,20 +13,26 @@ export function Pergunta({ pergunta, selected, setSelected }: Props) {
     <div
       className="flex flex-col lg:flex-row min-w-screen lg:min-h-screen snap-center snap-always"
     >
-      <div className="min-h-full lg:w-1/2 h-1/2 flex items-center justify-center">
-        <div className="my-auto bg-neutral-200 rounded-md p-4 w-5/6 h-11/12 aspect-square">
+      <div className="min-h-screen w-full lg:w-1/2 flex items-center justify-center p-4 ">
+        <div className="my-auto bg-neutral-200 rounded-md p-4 w-5/6 min-h-11/12">
           {pergunta.img_url &&
-            <Image
-              src={pergunta.img_url}
-              className="rounded-sm"
-              width={1200}
-              height={720}
-              alt="minato"
-              quality={100}
-              priority
-            />
+            <div className="aspect-square">
+              <Image
+                src={pergunta.img_url}
+                className="rounded-sm"
+                width={1200}
+                height={720}
+                alt=""
+                quality={100}
+                priority
+              />
+            </div>
           }
-          <p className="text-justify my-2 p-3 bg-white rounded-sm">Juninho irá iniciar seu primeiro dia na empresa Fazemos de Tudo e Mais um Pouco LTDA. Ele precisa chegar às 8h. Então ele acorda cedo, toma seu café da manhã e pega a condução às 7h para chegar no horário. Na empresa, Juninho conhece seus colegas e o time que irá fazer parte. Sua gerente, Alana o apresenta para o desenvolvedor Sênior que irá acompanhá-lo.</p>
+          <div className="p-3 bg-white rounded-sm">
+            <p className="">
+              Juninho irá iniciar seu primeiro dia na empresa Fazemos de Tudo e Mais um Pouco LTDA. Ele precisa chegar às 8h. Então ele acorda cedo, toma seu café da manhã e pega a condução às 7h para chegar no horário. Na empresa, Juninho conhece seus colegas e o time que irá fazer parte. Sua gerente, Alana o apresenta para o desenvolvedor Sênior que irá acompanhá-lo.
+            </p>
+          </div>
         </div>
       </div>
       <div className="lg:min-h-full min-h-1/2 lg:w-1/2 h-1/2 bg-neutral-800 p-20 text-center">
